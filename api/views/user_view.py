deb83d29fa0805af2ceaea8f50bb18e0d3002075
bisonlou <bisonlou@gmail.com>
2019-01-22 09:40:31 +0300

    Ft user can login (#7)
    * added user login route
    
    * added user login route

--- api/views/user_view.py
+++ api/views/user_view.py
@@ -5,8 +5,11 @@ user_controller = UserController()
 
 
 @app.route('/api/v1/auth/signup', methods=['POST'])
-def register_user():
-
+def register():
     return user_controller.register()
+
+@app.route('/api/v1/auth/login', methods=['POST'])
+def login():
+    return user_controller.login()
 
 

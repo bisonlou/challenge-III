66c5b4b64f7e3c21bc86323eb2a38f3a0fd599c3
Innocent Lou <bisonlou@gmail.com>
2019-01-31 10:58:30 +0300

    Added on load function to body

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,30 +1,33 @@
 function createNode(element) {
-    return document.createElement(element);
+  return document.createElement(element);
 }
 
 function append(parent, el) {
-  return parent.appendChild(el);
+return parent.appendChild(el);
 }
 
-const table = document.getElementById('redflag-table');
-const url = 'http://127.0.0.1:5000/api/v1/redflags';
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-  let redflag = data.results;
-  return authors.map(function(author) {
-    let tr = createNode('tr'),
-        td = createNode('td')
-
-    date = `${redflag.dteRegistered}`;
-    title = `${redflag.title}`;
-    location = `${redflag.location}`;
-
-    append(table, tr);
-    append(tr, td);
-    append(td, title);
+function loadRedFlags(){
+  const table = document.getElementById('redflag-table');
+  const url = 'http://127.0.0.1:5000/api/v1/redflags';
+  
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let redflag = data.results;
+    return authors.map(function(author) {
+      let tr = createNode('tr'),
+          td = createNode('td')
+  
+      date = `${redflag.dteRegistered}`;
+      title = `${redflag.title}`;
+      location = `${redflag.location}`;
+  
+      append(table, tr);
+      append(tr, td);
+      append(td, title);
+    })
   })
-})
-.catch(function(error) {
-    console.log(JSON.stringify(error));
-  });   
+  .catch(function(error) {
+      console.log(JSON.stringify(error));
+    });   
+}

9cf9872f2b3b126b942344f000b6ed608c67f3c2
Innocent Lou <bisonlou@gmail.com>
2019-02-01 16:10:45 +0300

    added scripts

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -32,27 +32,127 @@ function loadRedFlags(){
     });   
 }
 
-function register(){
-  url = 'http://127.0.0.1:5000/api/v1/auth/signup'
-  var data = {
-    "user_name": "innocent",
-    "email": "bisonlou@gmail.com",
-    "first_name": "lou",
-    "last_name": "lou",
-    "phone_number": "0753669897",
-    "password": "Pa$$word123",
-    "other_names": "",
-    "is_admin": false
-  };
+function welcome(){
+  url = 'http://127.0.0.1:5000/';
 
-  fetch(url, {
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  }).then(res => res.json())
-  .then(response => console.log('Success:', JSON.stringify(response)))
-  .catch(error => console.error('Error:', error));
-
+fetch(url)
+.then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
 }
+
+function register(){
+  url = 'http://127.0.0.1:5000/api/v1/auth/signup';
+
+  email = document.getElementById('email').value
+  username = document.getElementById('username').value
+  firstname = document.getElementById('firstname').value
+  lastname = document.getElementById('lastname').value
+  othernames = document.getElementById('othernames').value
+  phonenumber = document.getElementById('phonenumber').value
+  password = document.getElementById('password').value
+  
+
+fetch(url,{
+  method: 'post',
+  headers: {
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+        'user_name': username,
+        'email': email,
+        'first_name': firstname,
+        'last_name': lastname,
+        'phone_number': phonenumber,
+        'password': password,
+        'other_names': othernames,
+        'is_admin': true
+    })
+  })
+
+.then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+}
+
+function login(){
+  url = 'http://127.0.0.1:5000/api/v1/auth/login';
+
+  email = document.getElementById('login_email').value
+  password = document.getElementById('login_password').value
+
+fetch(url,{
+  method: 'post',
+  headers: {
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'},
+  body: JSON.stringify({
+        'email': email,
+        'password': password
+    })
+  })
+
+.then(response => {
+  return response.json();
+}).then(data => {
+  token = data['data'][0]['access_token'];
+  bearer_token = "Bearer " + token + ";";
+  document.cookie = "token=" + bearer_token;
+}).catch(err => {
+  console.log(err);
+});
+}
+
+
+function getRedflags(){
+  url = 'http://127.0.0.1:5000/api/v1/redflags';
+
+fetch(url,{
+  headers: {
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'},
+  headers: {
+        'Authorization': document.cookie
+    }
+  })
+
+.then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data)
+}).catch(err => {
+  console.log(err);
+});
+}
+
+function postRedflags(){
+  url = 'http://127.0.0.1:5000/api/v1/redflags';
+
+fetch(url,{
+  method: 'post',
+  headers: {
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'},
+  headers: {
+        'Authorization': document.cookie
+    }
+  })
+
+.then(response => {
+  return response.json();
+}).then(data => {
+  console.log(data)
+}).catch(err => {
+  console.log(err);
+});
+}
+

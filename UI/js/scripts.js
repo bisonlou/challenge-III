7b39027f3b94e1e6d0d11997a8a3738fd430f1b8
Innocent Lou <bisonlou@gmail.com>
2019-02-02 20:17:42 +0300

    Added get interventions

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -78,7 +78,7 @@ function login(){
     document.cookie = "token=" + bearer_token;
 
     if (data['status'] == 200){
-      window.location.href = "http://localhost/iReporter/home.html";
+      window.location.href = "http://localhost/iReporter/index.html";
     }
   }).catch(err => {
     console.log(err);
@@ -115,10 +115,92 @@ function getRedflags(){
           var cell2 = row.insertCell(1);
           var cell3 = row.insertCell(2);
           var cell4 = row.insertCell(3);
-          cell1.innerHTML = data['data'][0][i]['createdon'];
+
+    incidentId = data['data'][0][i]['id'];
+	  long_date_time = new Date(data['data'][0][i]['createdon']);
+		
+	  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	  var days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+
+	    month = months[long_date_time.getMonth()];
+	    day = days[long_date_time.getDay()];
+	    year = long_date_time.getFullYear();
+      date = long_date_time.getDate();
+        if(date < 10){
+          date = "0" + date;
+        }
+
+      display_date = day + " " + date + " " + month + " " + year;
+      
+	
+          cell1.innerHTML = display_date;
           cell2.innerHTML =  data['data'][0][i]['title'];
           cell3.innerHTML =  data['data'][0][i]['status'];
-          cell4.innerHTML =  '<a href="./redflag_edit.html">Edit</a> | <a href="./redflag_confirm_delete.html">Delete</a>';
+          cell4.innerHTML =  '<a href="./incident_edit.html?type=red-flag&id=' + incidentId +
+                             '">Edit</a> | <a href="./incident_confirm_delete.html?type=red-flag&id=' + incidentId +
+                             '">Delete</a>';
+
+    }
+  
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+function getInterventions(){
+  url = 'http://127.0.0.1:5000/api/v1/interventions';
+
+  fetch(url,{
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'},
+    headers: {
+          'Authorization': document.cookie
+      }
+    })
+
+  .then(response => {
+    return response.json();
+  }).then(data => {
+
+    if (data['status'] == 401){
+      window.location.href = "http://localhost/iReporter/login.html";
+    }
+    
+    redflags_table = document.getElementById('intervention-table');
+
+    for(i=0; i< (data['data'][0]).length; i++){
+          var row = redflags_table.insertRow(i + 1);
+          
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+
+	  incidentId = data['data'][0][i]['id'];
+	  long_date_time = new Date(data['data'][0][i]['createdon']);
+		
+	  var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	  var days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+
+	    month = months[long_date_time.getMonth()];
+	    day = days[long_date_time.getDay()];
+	    year = long_date_time.getFullYear();
+      date = long_date_time.getDate();
+        if(date < 10){
+          date = "0" + date;
+        }
+
+      display_date = day + " " + date + " " + month + " " + year;
+      	
+          cell1.innerHTML = display_date;
+          cell2.innerHTML =  data['data'][0][i]['title'];
+          cell3.innerHTML =  data['data'][0][i]['status'];
+          cell4.innerHTML =  '<a href="./incident_edit.html?type=intervention&id=' + incidentId +
+                             '">Edit</a> | <a href="./incident_confirm_delete.html?type=intervention&id=' + incidentId +
+                             '">Delete</a>';
 
     }
   
@@ -128,7 +210,8 @@ function getRedflags(){
 }
 
 
-function postRedflags(){
+
+function postIncident(incidentType){
   url = 'http://127.0.0.1:5000/api/v1/incidents';
 
   title = document.getElementById('title').value
@@ -140,7 +223,7 @@ function postRedflags(){
           'title': title,
           'comment': comment,
           'location': '(00.000000, 00.000000)',
-          'type': 'red-flag',
+          'type': incidentType,
           'status': 'pending',
           'images': ['image_001.jpg'],
           'videos': ['video_001.mp4']
@@ -159,10 +242,48 @@ function postRedflags(){
     return response.json();
   }).then(data => {
     if (data['status'] == 201){
-      window.location.href = "http://localhost/iReporter/home.html";
+      window.location.href = "http://localhost/iReporter/index.html";
     }
   }).catch(err => {
     console.log(err);
   });
 }
 
+function getIncident(){
+  var url = window.location.href;
+  var incidentType = /type=([^&]+)/.exec(url)[1];
+  var incidentId = /id=([^&]+)/.exec(url)[1];
+  incidentId = parseInt(incidentId, 10)
+
+  title = document.getElementById('title')
+  comment = document.getElementById('comment')
+
+  url = 'http://127.0.0.1:5000/api/v1/incidents/' + incidentId;
+
+  fetch(url,{
+    method: 'get',
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'},
+    headers: {
+          'Authorization': document.cookie,
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+      }
+    })
+
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    if (data['status'] == 201){
+      window.location.href = "http://localhost/iReporter/index.html";
+    }
+
+    title.innerHTML = data['data'][0]['title']
+    comment.innerHTML = data['data'][0]['comment']
+
+  }).catch(err => {
+    console.log(err);
+  });
+
+}

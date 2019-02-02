2e489d9550d8e01060b129488e6085f1df386630
Innocent Lou <bisonlou@gmail.com>
2019-02-02 17:16:45 +0300

    renamed home to index

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -1,48 +1,15 @@
-function createNode(element) {
-  return document.createElement(element);
-}
-
-function append(parent, el) {
-return parent.appendChild(el);
-}
-
-function loadRedFlags(){
-  const table = document.getElementById('redflag-table');
-  const url = 'http://127.0.0.1:5000/api/v1/redflags';
-  
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-    let redflag = data.results;
-    return authors.map(function(author) {
-      let tr = createNode('tr'),
-          td = createNode('td')
-  
-      date = `${redflag.dteRegistered}`;
-      title = `${redflag.title}`;
-      location = `${redflag.location}`;
-  
-      append(table, tr);
-      append(tr, td);
-      append(td, title);
-    })
-  })
-  .catch(function(error) {
-      console.log(JSON.stringify(error));
-    });   
-}
 
 function welcome(){
   url = 'http://127.0.0.1:5000/';
 
-fetch(url)
-.then(response => {
-  return response.json();
-}).then(data => {
-  console.log(data);
-}).catch(err => {
-  console.log(err);
-});
+  fetch(url)
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 function register(){
@@ -57,33 +24,33 @@ function register(){
   password = document.getElementById('password').value
   
 
-fetch(url,{
-  method: 'post',
-  headers: {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-        'user_name': username,
-        'email': email,
-        'first_name': firstname,
-        'last_name': lastname,
-        'phone_number': phonenumber,
-        'password': password,
-        'other_names': othernames,
-        'is_admin': true
+  fetch(url,{
+    method: 'post',
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+          'user_name': username,
+          'email': email,
+          'first_name': firstname,
+          'last_name': lastname,
+          'phone_number': phonenumber,
+          'password': password,
+          'other_names': othernames,
+          'is_admin': true
+      })
     })
-  })
 
-.then(response => {
-  return response.json();
-}).then(data => {
-  if (data['status'] == 201){
-    window.location.href = "http://localhost/iReporter/login.html";
-  }
-}).catch(err => {
-  console.log(err);
-});
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    if (data['status'] == 201){
+      window.location.href = "http://localhost/iReporter/login.html";
+    }
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 function login(){
@@ -92,68 +59,72 @@ function login(){
   email = document.getElementById('login_email').value
   password = document.getElementById('login_password').value
 
-fetch(url,{
-  method: 'post',
-  headers: {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json'},
-  body: JSON.stringify({
-        'email': email,
-        'password': password
+  fetch(url,{
+    method: 'post',
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'},
+    body: JSON.stringify({
+          'email': email,
+          'password': password
+      })
     })
-  })
 
-.then(response => {
-  return response.json();
-}).then(data => {
-  token = data['data'][0]['access_token'];
-  bearer_token = "Bearer " + token + ";";
-  document.cookie = "token=" + bearer_token;
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    token = data['data'][0]['access_token'];
+    bearer_token = "Bearer " + token + ";";
+    document.cookie = "token=" + bearer_token;
 
-  if (data['status'] == 200){
-    window.location.href = "http://localhost/iReporter/home.html";
-  }
-}).catch(err => {
-  console.log(err);
-});
+    if (data['status'] == 200){
+      window.location.href = "http://localhost/iReporter/home.html";
+    }
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 
 function getRedflags(){
-url = 'http://127.0.0.1:5000/api/v1/redflags';
+  url = 'http://127.0.0.1:5000/api/v1/redflags';
 
-fetch(url,{
-  headers: {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json'},
-  headers: {
-        'Authorization': document.cookie
+  fetch(url,{
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'},
+    headers: {
+          'Authorization': document.cookie
+      }
+    })
+
+  .then(response => {
+    return response.json();
+  }).then(data => {
+
+    if (data['status'] == 401){
+      window.location.href = "http://localhost/iReporter/login.html";
     }
-  })
+    
+    redflags_table = document.getElementById('redflag-table');
 
-.then(response => {
-  return response.json();
-}).then(data => {
+    for(i=0; i< (data['data'][0]).length; i++){
+          var row = redflags_table.insertRow(i + 1);
+          
+          var cell1 = row.insertCell(0);
+          var cell2 = row.insertCell(1);
+          var cell3 = row.insertCell(2);
+          var cell4 = row.insertCell(3);
+          cell1.innerHTML = data['data'][0][i]['createdon'];
+          cell2.innerHTML =  data['data'][0][i]['title'];
+          cell3.innerHTML =  data['data'][0][i]['status'];
+          cell4.innerHTML =  '<a href="./redflag_edit.html">Edit</a> | <a href="./redflag_confirm_delete.html">Delete</a>';
+
+    }
   
-  redflags_table = document.getElementById('redflag-table');
-
-  for(i=0; i< (data['data'][0]).length; i++){
-        var row = redflags_table.insertRow(i + 1);
-        
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        cell1.innerHTML = data['data'][0][i]['createdon'];
-        cell2.innerHTML =  data['data'][0][i]['title'];
-        cell3.innerHTML =  data['data'][0][i]['status'];
-        cell4.innerHTML =  '<a href="./redflag_edit.html">Edit</a> | <a href="./redflag_confirm_delete.html">Delete</a>';
-
-  }
- 
-}).catch(err => {
-  console.log(err);
-});
+  }).catch(err => {
+    console.log(err);
+  });
 }
 
 
@@ -163,35 +134,35 @@ function postRedflags(){
   title = document.getElementById('title').value
   comment = document.getElementById('comment').value
 
-fetch(url,{
-  method: 'post',
-  body: JSON.stringify({
-        'title': title,
-        'comment': comment,
-        'location': '(00.000000, 00.000000)',
-        'type': 'red-flag',
-        'status': 'pending',
-        'images': ['image_001.jpg'],
-        'videos': ['video_001.mp4']
-       }),
-  headers: {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json'},
-  headers: {
-        'Authorization': document.cookie,
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-    }
-  })
+  fetch(url,{
+    method: 'post',
+    body: JSON.stringify({
+          'title': title,
+          'comment': comment,
+          'location': '(00.000000, 00.000000)',
+          'type': 'red-flag',
+          'status': 'pending',
+          'images': ['image_001.jpg'],
+          'videos': ['video_001.mp4']
+        }),
+    headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json'},
+    headers: {
+          'Authorization': document.cookie,
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+      }
+    })
 
-.then(response => {
-  return response.json();
-}).then(data => {
-  if (data['status'] == 201){
-    window.location.href = "http://localhost/iReporter/home.html";
-  }
-}).catch(err => {
-  console.log(err);
-});
+  .then(response => {
+    return response.json();
+  }).then(data => {
+    if (data['status'] == 201){
+      window.location.href = "http://localhost/iReporter/home.html";
+    }
+  }).catch(err => {
+    console.log(err);
+  });
 }
 

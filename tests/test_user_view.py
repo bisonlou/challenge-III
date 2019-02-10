f9ff25e419c1190e50f161742df7dfce266bada8
bisonlou <bisonlou@gmail.com>
2019-02-10 23:10:52 +0300

    Added get users endpoint (#23)
    * Ft user can register (#2)
    
    * Added initial setup files
    
    * initial commit
    
    * chaged repo token
    
    * Added create relation script
    
    * added badges
    
    * Ft user can register (#3)
    
    * Added initial setup files
    
    * initial commit
    
    * chaged repo token
    
    * Added create relation script
    
    * added badges
    
    * required gunicorn
    
    * Ft user can register (#4)
    
    * Added initial setup files
    
    * initial commit
    
    * chaged repo token
    
    * Added create relation script
    
    * added badges
    
    * required gunicorn
    
    * chaged sqlalchemy uri
    
    * Ft user can register (#6)
    
    * Added initial setup files
    
    * initial commit
    
    * chaged repo token
    
    * Added create relation script
    
    * added badges
    
    * required gunicorn
    
    * chaged sqlalchemy uri
    
    * removed sqlalchemy
    
    * Modified connection string
    
    * Ft user can login (#7)
    
    * added user login route
    
    * added user login route
    
    * Ft user can login (#9)
    
    * added user login route
    
    * added user login route
    
    * refactored login validation
    
    * Ft user can view red flag (#10)
    
    * Added initial setup files
    
    * Initial commit
    
    * changed connection string
    
    * fixed travis bug
    
    * Initial commit
    
    * Initial commit
    
    * Fixed travis error
    
    * changed travis badge
    
    * Added more endpoints
    
    * Changed connection string
    
    * Ft user can view red flag (#11)
    
    * Added initial setup files
    
    * Initial commit
    
    * changed connection string
    
    * fixed travis bug
    
    * Initial commit
    
    * Initial commit
    
    * Fixed travis error
    
    * changed travis badge
    
    * Added more endpoints
    
    * Changed connection string
    
    * Used enviroment variables
    
    *  fixed heroku error
    
    * Ft user can delete red flag (#12)
    
    *  fixed heroku error
    
    * Added jwt protection
    
    * remove flask-jwt from requirements
    
    * chaged tests
    
    * Worked on validation
    
    * Ft admin can change red flags (#13)
    
    *  fixed heroku error
    
    * admin can chage status
    
    * admin can chage status
    
    * added init file
    
    * changed file name
    
    * removed commented out code (#14)
    
    * Create README.md
    
    * added environment variables required (#15)
    
    * added environment variables required
    
    * edited spelling error (#16)
    
    * Update README.md
    
    * refactored validators
    
    * removed user validators
    
    * removed incident validators
    
    * removed incident validator
    
    * Added post fetch api
    
    * Added login and register scripts
    
    * removed UI dir
    
    * refactored incident model
    
    * Added more tests
    
    * pip froze requirement
    
    * Hooked create redflag UI to API
    
    * Changed create_db.sql
    
    * Added get users
    
    * modified tests

--- tests/test_user_view.py
+++ tests/test_user_view.py
@@ -1,118 +1,47 @@
 import unittest
-import json
-from api import app, test_client
+from tests.test_base import TestBase
 from api.models.user_model import User
 from api.database.engine import DbConnection
+
+base = TestBase()
 
 
 class TestUserView(unittest.TestCase):
 
     def setUp(self):
         """
-        Setup test client
+        Setup database connection
         """
-        self.test_client = test_client
         self.db_services = DbConnection()
 
     def tearDown(self):
         """
-        teardown test client
+        teardown database
         """
         self.db_services.reset_database()
 
     def test_register_user_succesfuly(self):
         """
         Test registering a user succesfuly
-        """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlou@gmail.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123',
-            'other_names': 'innocent',
-            'is_admin': True
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
-
+        """    
+        response = base.register_user()
         self.assertEqual(response.status_code, 201)
 
     def test_register_user_with_blank_password(self):
         """
         Test registering a user without a password
         """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlou@aol.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': '',
-            'other_names': '',
-            'is_admin': True
-
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        data = {'password': ''}
+        response = base.register_user(data)
 
         self.assertEqual(response.status_code, 400)
-
-    def test_register_user_with_missing_keys(self):
-        """
-        Test registering a user with missing email
-        """
-        user = {
-            'user_name': 'bison',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123',
-            'other_names': '',
-            'is_admin': True
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
-
-        self.assertEqual(response.status_code, 400)
-
+   
     def test_register_user_with_blank_firstname(self):
         """
         Test registering a user with a blank first name
         """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlou@gmail.com',
-            'first_name': '',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': '',
-            'other_names': '',
-            'is_admin': True
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        data = {'first_name': ''}
+        response = base.register_user(data)
 
         self.assertEqual(response.status_code, 400)
 
@@ -120,23 +49,8 @@ class TestUserView(unittest.TestCase):
         """
         Test registering a user with password longer than 12 characters
         """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlou@gmail.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123456',
-            'other_names': '',
-            'is_admin': True
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        data = {'password': 'Pa$$word123456'}
+        response = base.register_user(data)
 
         self.assertEqual(response.status_code, 400)
 
@@ -144,23 +58,8 @@ class TestUserView(unittest.TestCase):
         """
         Test registering a user with an invalid email
         """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlougmail.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123',
-            'other_names': 'innocent',
-            'is_admin': True
-        }
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        data = {'email': 'bisonlougmail.com'}
+        response = base.register_user(data)
 
         self.assertEqual(response.status_code, 400)
 
@@ -168,58 +67,42 @@ class TestUserView(unittest.TestCase):
         """
         Test registering a user a second time
         """
-        user = {
-            'user_name': 'bison',
-            'email': 'bisonlou@gmail.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123',
-            'other_names': 'innocent',
-            'is_admin': True
-        }
-
-        self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        base.register_user()
+        response = base.register_user()
 
         self.assertEqual(response.status_code, 409)
 
     def test_register_with_improper_type(self):
         """
-        Test registering a user a second time
+        Test registering a user name 
         """
-        user = {
-            'user_name': 123,
-            'email': 'bisonlou@gmail.com',
-            'first_name': 'bison',
-            'last_name': 'lou',
-            'phone_number': '0753669897',
-            'password': 'Pa$$word123',
-            'other_names': 'innocent',
-            'is_admin': True
-        }
-
-        self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        response = self.test_client.post(
-            '/api/v1/auth/signup',
-            content_type='application/json',
-            data=json.dumps(user))
-
-        message = json.loads(response.data)
+        data = {'user_name': 123}
+        response = base.register_user(data)
 
         self.assertEqual(response.status_code, 400)
 
+    def test_get_user(self):
+        """
+        Test getting one user
+        """
+        credentials = {'email': 'bisonlou@gmail.com',
+                       'password': 'Pa$$word123'}
+        token = base.get_token(credentials)
+        response = base.get_users(token)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_users(self):
+        """
+        Test getting one user as admin
+        """
+        credentials = {'email': 'bisonlou@gmail.com',
+                       'password': 'Pa$$word123'}
+        data = {'is_admin': True}
+        response = base.register_user(data)
+
+        token = base.get_token(credentials)
+        response = base.get_users(token)
+
+        self.assertEqual(response.status_code, 200)
     
